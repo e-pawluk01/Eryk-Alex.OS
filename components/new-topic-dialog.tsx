3de7d6b30b7401cbb6f9c1a75e61bb84bdd7c03d
e-pawluk01 <eryk.pawluk01@gmail.com>
2026-08-02 16:08:43 +0100

    fix: import useEffect in new-topic-dialog

--- components/new-topic-dialog.tsx
+++ components/new-topic-dialog.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { ContextType, Topic } from "@/lib/types";
 import { Plus, X } from "lucide-react";
 import { supabase } from "@/lib/supabase";
