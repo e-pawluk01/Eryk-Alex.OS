111efa89f6601b3f9a3973b5a44e3e9c6f491185
e-pawluk01 <eryk.pawluk01@gmail.com>
2026-08-26 19:05:52 +0100

    feat: add Development and Content task types

--- components/clock-in-dialog.tsx
+++ components/clock-in-dialog.tsx
@@ -6,6 +6,8 @@ import { supabase } from "@/lib/supabase";
 import { useGlobalContext } from "./global-context";
 
 const TASKS = [
+  "Development",
+  "Content",
   "Sourcing",
   "Cleaning / Restoration",
   "Photography",
